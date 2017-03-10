26272564de9c62a50d57221bd906460486e56f30
peyote2504 <maratovna362@gmail.com>
2017-03-11 04:33:11 +0600

    mid in class

---
+++ Polygon/Polygon/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Polygon
+{
+    class Polygon1
+    {
+        public List<Point> line;
+    }
+    class Program
+    {
+        static void emptySpace(int level)
+        {
+            for (int i = 0; i < level * 2; i++)
+                Console.Write(" ");
+        }
+
+        static void Polygon(string polygon, int level)
+        {
+            {
+                if (level > 2)
+                    return;
+                try
+                {
+                    DirectoryInfo directory = new DirectoryInfo(polygon);
+                    FileInfo[] files = directory.GetFiles();
+                    DirectoryInfo[] directories = directory.GetDirectories();
+
+                    foreach (FileInfo file in files)
+                    {
+                        emptySpace(level);
+                        Console.WriteLine(file.Name);
+                    }
+                    foreach (DirectoryInfo dInfo in directories)
+                    {
+                        emptySpace(level);
+                        Console.WriteLine(dInfo.Name);
+                        Polygon(dInfo.FullName, level + 1);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ sum/sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = 0;
+            string n = Console.ReadLine();
+            int p = Int32.Parse(n);
+            while (p > 0)
+            {
+                s = s + (p % 10);
+                p = p / 10;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
